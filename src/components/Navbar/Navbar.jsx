5b63e6dbe6be2187917c12ae1ed13fb0a3e17a5b
Kevin <kevinsotozabaleta@gmail.com>
2024-07-25 11:30:58 -0400

    Primer Entrega1

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+
+    return(
+        <>
+            <nav className="navbar">
+
+                <div className="navbar_a">
+
+                    <div className="navbar-link">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Zapatillas</a></li>
+                        <li><a href="#">Poleron</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        
+                    </div>
+
+                </div>
+
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
